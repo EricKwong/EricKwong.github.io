28f454136b3f5686dc27cc19838da80366dbdee8
Eric Kwong <erickwong618@yahoo.com>
2015-05-13 10:29:09 -0400

    initial commit

--- location.js
+++ location.js
@@ -11,15 +11,15 @@ $(function() {
     var lonFeet = Math.round(lonKm * 3280.4);
 
     var steps = 0;
-    var lonDiff = Math.abs(self.props.currentLon) - Math.abs(lonFeet);
-    var latDiff = Math.abs(self.props.currentLat) - Math.abs(latFeet);
-    if (Math.abs(lonDiff) > 0 && Math.abs(lonDiff) < 200) {
-      steps += Math.abs(lonDiff);
-    }
+    // var lonDiff = Math.abs(self.props.currentLon) - Math.abs(lonFeet);
+    // var latDiff = Math.abs(self.props.currentLat) - Math.abs(latFeet);
+    // if (Math.abs(lonDiff) > 0 && Math.abs(lonDiff) < 200) {
+    //   steps += Math.abs(lonDiff);
+    // }
 
-    if (Math.abs(latDiff) > 0 && Math.abs(latDiff) < 200) {
-      steps += Math.abs(latDiff);
-    }
+    // if (Math.abs(latDiff) > 0 && Math.abs(latDiff) < 200) {
+    //   steps += Math.abs(latDiff);
+    // }
 
     $("#steps").text(latFeet + ' ' + lonFeet);
   })
