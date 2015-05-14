8981dfb60b94f469e120f12cca65681bdbac3d33
Eric Kwong <erickwong618@yahoo.com>
2015-05-14 09:25:54 -0400

    integrated steps

--- location.js
+++ location.js
@@ -1,5 +1,9 @@
 $(function() {
   var self = this;
+  var steps = 0;
+  var currentLon;
+  var currentLat;
+
   navigator.geolocation.watchPosition(function(position) {
     var lat = position.coords.latitude;
     var lon = position.coords.longitude;
@@ -7,20 +11,20 @@ $(function() {
     var latKm = lat * (10000/90);
     var lonKm = lon * (10000/90);
 
-    var latFeet = Math.round(latKm * 3280.4);
-    var lonFeet = Math.round(lonKm * 3280.4);
+    var latFeet = Math.abs(Math.round(latKm * 3280.4));
+    var lonFeet = Math.abs(Math.round(lonKm * 3280.4));
 
-    var steps = 0;
-    // var lonDiff = Math.abs(self.props.currentLon) - Math.abs(lonFeet);
-    // var latDiff = Math.abs(self.props.currentLat) - Math.abs(latFeet);
-    // if (Math.abs(lonDiff) > 0 && Math.abs(lonDiff) < 200) {
-    //   steps += Math.abs(lonDiff);
-    // }
+    var lonDiff = currentLon - lonFeet;
+    var latDiff = currentLat - latFeet;
+    
+    if (Math.abs(lonDiff) > 0 && Math.abs(lonDiff) < 200) {
+      steps += Math.abs(lonDiff);
+    }
 
-    // if (Math.abs(latDiff) > 0 && Math.abs(latDiff) < 200) {
-    //   steps += Math.abs(latDiff);
-    // }
+    if (Math.abs(latDiff) > 0 && Math.abs(latDiff) < 200) {
+      steps += Math.abs(latDiff);
+    }
 
-    $("#steps").text(latFeet + ' ' + lonFeet);
+    $("#steps").text(steps);
   })
 });
