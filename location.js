cb04eda21e855b9e440468ec087f83b57b8580e5
Eric Kwong <erickwong618@yahoo.com>
2015-06-04 15:29:20 -0400

    made calculating steps more precise

--- location.js
+++ location.js
@@ -43,7 +43,7 @@ $(function() {
 
     if (currentLat != null && currentLon != null) {
       var newStep = calculateDistance(currentLat, currentLon, lat, lon);
-      steps += Math.round((newStep * 3280.84) / 2) ; 
+      steps += Math.round((newStep * 3280.84) / 2.8) ; 
       currentLat = lat;
       currentLon = lon;     
     } else {
