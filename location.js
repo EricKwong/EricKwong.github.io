4444f50ce5daf176ce81e80133394869ccce67f5
Eric Kwong <erickwong618@yahoo.com>
2015-06-04 13:04:42 -0400

    just showing coords

--- location.js
+++ location.js
@@ -8,28 +8,28 @@ $(function() {
     var lat = position.coords.latitude;
     var lon = position.coords.longitude;
 
-    var latKm = lat * (10000/90);
-    var lonKm = lon * (10000/90);
+    // var latKm = lat * (10000/90);
+    // var lonKm = lon * (10000/90);
 
-    var latFeet = Math.abs(Math.round(latKm * 3280.4));
-    var lonFeet = Math.abs(Math.round(lonKm * 3280.4));
+    // var latFeet = Math.abs(Math.round(latKm * 3280.4));
+    // var lonFeet = Math.abs(Math.round(lonKm * 3280.4));
 
-    if (currentLon != null && currentLat !=null) {
-      var lonDiff = currentLon - lonFeet;
-      var latDiff = currentLat - latFeet;
+    // if (currentLon != null && currentLat !=null) {
+    //   var lonDiff = currentLon - lonFeet;
+    //   var latDiff = currentLat - latFeet;
       
-      if (Math.abs(lonDiff) > 0 && Math.abs(lonDiff) < 200) {
-        steps += Math.abs(lonDiff);
-      }
+    //   if (Math.abs(lonDiff) > 0 && Math.abs(lonDiff) < 200) {
+    //     steps += Math.abs(lonDiff);
+    //   }
 
-      // if (Math.abs(latDiff) > 0 && Math.abs(latDiff) < 200) {
-      //   steps += Math.abs(latDiff);
-      // }
-    }
+    //   // if (Math.abs(latDiff) > 0 && Math.abs(latDiff) < 200) {
+    //   //   steps += Math.abs(latDiff);
+    //   // }
+    // }
 
-    currentLon = lonFeet;
-    currentLat = latFeet;
+    // currentLon = lonFeet;
+    // currentLat = latFeet;
 
-    $("#steps").text(steps);
+    $("#steps").text(lat + ", " + lon);
   })
 });
