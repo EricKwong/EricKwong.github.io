5fb37a7e5745b6dd44b3a314890272f7d8eec63c
Eric Kwong <erickwong618@yahoo.com>
2015-06-04 14:41:03 -0400

    added High Accuracy

--- location.js
+++ location.js
@@ -1,8 +1,6 @@
 $(function() {
   // var self = this;
-  // var steps = 0;
-  // var currentLon;
-  // var currentLat;
+
 
   // navigator.geolocation.watchPosition(function(position) {
   //   var lat = position.coords.latitude;
@@ -33,26 +31,41 @@ $(function() {
   //   $("#steps").text(lat + ", " + lon);
   // })
 
+  var steps = 0;
+  var currentLon;
+  var currentLat;
 
+  var success = function (position) {
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
 
-navigator.geolocation.watchPosition(success ,error,
-{ maximumAge: 0, timeout: 5000, enableHighAccuracy: true } );
+    var latKm = lat * (10000/90);
+    var lonKm = lon * (10000/90);
 
+    var latFeet = Math.abs(latKm * 3280.4);
+    var lonFeet = Math.abs(lonKm * 3280.4);
 
+    if (currentLat != null && currentLon != null) {
+      var latStep = Math.abs(currentLat - latFeet);
+      var lonStep = Math.abs(currentLon - lonFeet);
+      step = step + latStep + lonStep;
+    } else {
+      currentLat = latFeet;
+      currentLon = lonFeet;
+    }
 
+    $("#steps").text(steps);  
+  };
+
+  var error = function (error) {
+    if (error.code === PositionError.TIMEOUT) {
+      navigator.geolocation.getCurrentPosition(success, error,
+      { maximumAge: 3000, timeout: 20000, enableHighAccuracy: false } );
+    }
+  };
+
+  navigator.geolocation.watchPosition(success ,error,
+  { maximumAge: 0, timeout: 5000, enableHighAccuracy: true } );
 
 });
 
-var success = function (position) {
-  var lat = position.coords.latitude;
-  var lon = position.coords.longitude;
-
-  $("#steps").text(lat + ", " + lon);  
-};
-
-var error = function (error) {
-  if (error.code === PositionError.TIMEOUT) {
-    navigator.geolocation.getCurrentPosition(success, error,
-    { maximumAge: 3000, timeout: 20000, enableHighAccuracy: false } );
-  }
-};
